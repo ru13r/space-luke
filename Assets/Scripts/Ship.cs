e444d7d45f4fbe29e147602983e6e1904065aaf2
ru13r <31513253+ru13r@users.noreply.github.com>
2023-10-19 12:09:38 +0200

    Remove size logic from Ship

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -7,9 +7,6 @@ public class Ship: MonoBehaviour
 {
     public WeaponSystem WeaponSystem => weaponSystem;
     [SerializeField] private GameObject weaponSystemObject;
-    // size and position
-    private Collider2D _collider;
-    private Vector3 _size;
     [SerializeField] private WeaponSystem weaponSystem;
     private HitAnimation _hitAnimation;
     
@@ -19,14 +16,8 @@ public class Ship: MonoBehaviour
     {
         weaponSystem = weaponSystemObject.GetComponent<WeaponSystem>();
         _hitAnimation = gameObject.AddComponent<HitAnimation>();
-        _collider = gameObject.GetComponent<Collider2D>();
-        _size = _collider.bounds.size;
         _health = maxHealth;
     }
-    
-
- 
-    public (float, float) GetDimensions() => (_size.x, _size.y);
 
     public void Damage(int damage)
     {
